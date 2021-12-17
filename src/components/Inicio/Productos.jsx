60a79b1a34aefbd3c6192f6b2c605f7d14883504
Cristian Duvan Machado Mosquera <cristiankm408@gmail.com>
2021-12-16 19:12:53 -0500

    cambios

--- src/components/Inicio/Productos.jsx
+++ src/components/Inicio/Productos.jsx
@@ -2,7 +2,7 @@
  *  @decs import libs
  */
 import React from "react";
-import dataProductos from '../json/data.json';
+import dataProductos from '../../json/data.json';
 
 export function Productos() {
     const elemnt_productos = (Object.values(dataProductos)[3])
